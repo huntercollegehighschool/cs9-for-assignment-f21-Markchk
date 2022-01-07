c78e2610889e14b7ba58a49cef28eb17521348cd
Mark Chklovskii <81195079+Markchk@users.noreply.github.com>
2022-01-07 06:33:58 +0000

    done

--- part3.py
+++ part3.py
@@ -19,3 +19,12 @@ Enter the height: 3
 """
 
 #write your code here 
+base = int(input("Enter the base: "))
+height = int(input("Enter the height"))
+square=''
+row = ''
+for x in range(1, base +1):
+  row = row +'*'
+for y in range(1, height +1):
+  square= square + f'\n {row}'
+print(square)
