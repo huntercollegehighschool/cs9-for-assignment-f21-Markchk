4aa15989d6717e3b2da4ce38a05c1be564931257
Mark Chklovskii <81195079+Markchk@users.noreply.github.com>
2022-01-07 06:36:42 +0000

    done, also i think this is from a udemy python course

--- part4.py
+++ part4.py
@@ -38,5 +38,17 @@ buzz
 fizz
 
 """
-
+def buzz_sorter(x):
+    if x % 3 == 0 and x % 5 == 0:
+        print('FizzBuzz')
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print('Buzz')
+    else:
+        print(x)
+    
+        
+for x in range(1,101):
+    buzz_sorter(x)
 # write your code here 
